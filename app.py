e9e2ad52d116f6a2527f43b91cea22ed424e069c
Jared Blitzstein <mailing-list@blitzstein.net>
2022-01-19 09:35:02 -0500

    added simple uuid

--- app.py
+++ app.py
@@ -1,17 +1,18 @@
 from flask import Flask
 from flask import render_template
+from datetime import datetime
 import gunicorn
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return render_template('index.html')
+    return render_template('index.html', id=datetime.now())
 
 @app.route("/feature-a")
 def hello_feature_a():
-    return render_template('feature-a.html')
+    return render_template('feature-a.html', id=datetime.now())
 
 @app.route("/feature-b")
 def hello_feature_b():
-    return render_template('feature-b.html')
+    return render_template('feature-b.html', id=datetime.now())
